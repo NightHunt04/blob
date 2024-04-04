dda843e1e462b726b5566fd27858f66a270c1860
admin1334 <jeetbherwani2004@gmail.com>
2024-04-04 18:49:04 +0530

    Fixing logout button on large devices

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -6,8 +6,8 @@ function Settings() {
 
     return (
         <div className={`setting ${settings ? 'w-[300px] md:w-[400px]' : 'w-0'} ${lightTheme ? 'text-[#343434] bg-[#cecece]' : 'text-[#e6e6e6] bg-[#363636]'} font-inter text-[19px] overflow-hidden absolute transition-all shadow-2xl h-screen right-0 top-0 z-20`}>
-            <div className={`flex transition-all relative flex-col items-center justify-center w-full pt-[80px] md:pt-[120px]`}>
-                <button className="text-[14px] md:text-[16px] px-4 py-1 md:px-5 md:py-2 bg-[#a40e00e7] shadow-2xl hover:bg-[#820000] mt-[650px] md:mt-[870px] text-white rounded-[30px]" onClick={toggleLogoutPopUp}>Logout &nbsp;<i className="fa-solid fa-arrow-right-from-bracket text-gray-200"></i></button>
+            <div className={`flex transition-all relative flex-col items-center justify-center w-full h-full pt-[80px] md:pt-[120px]`}>
+                <button className="absolute text-[14px] md:text-[16px] px-4 py-1 md:px-5 md:py-2 bg-[#a40e00e7] shadow-2xl hover:bg-[#820000] bottom-10 text-white rounded-[30px]" onClick={toggleLogoutPopUp}>Logout &nbsp;<i className="fa-solid fa-arrow-right-from-bracket text-gray-200"></i></button>
             </div>
         </div>
     )
