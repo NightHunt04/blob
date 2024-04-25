a7c8332e9a99eb5f6073d707bb2c18fc71bab773
admin1334 <jeetbherwani2004@gmail.com>
2024-04-25 16:48:08 +0530

    Logout component update

--- src/components/LogoutPopUp/LogoutPopUp.jsx
+++ src/components/LogoutPopUp/LogoutPopUp.jsx
@@ -16,13 +16,13 @@ function LogoutPopUp() {
     }
 
     return (
-        <div className={`${logoutPopUp ? 'translate-y-[80px] md:translate-y-[180px]' : '-translate-y-[1000px]'} transition-all text-[15px] md:text-[17px] font-inter z-40 w-[300px] md:w-[730px] ${lightTheme ? 'bg-white' : 'bg-[#292929]'} shadow-2xl  mx-auto top-[300px] rounded-xl`}>
-            <div className="w-full h-full flex flex-col items-center justify-start text-gray-300">
+        <div className={`${logoutPopUp ? 'translate-y-[80px] md:translate-y-[180px]' : '-translate-y-[1000px]'} transition-all text-[15px] md:text-[17px] font-inter z-40 w-[300px] md:w-[730px] ${lightTheme ? 'bg-gray-100 text-gray-900' : 'bg-[#292929] text-gray-300'} shadow-2xl  mx-auto top-[300px] rounded-xl`}>
+            <div className="w-full h-full flex flex-col items-center justify-start">
                 <p className="pt-5 px-4">Are you sure, you want to logout from the account <span className="text-[#d60000]">{currentUserEmail}</span>?</p>
                 <p className="px-4">You will be redirected back to the home/landing page.</p>
                 <div className="flex items-center justify-center gap-3 py-6">
-                    <button className="px-4 md:px-10 py-1 bg-[#a90000] rounded-[28px] hover:opacity-90" onClick={toggleLogoutPopUp}>Cancel</button>
-                    <button className="px-4 md:px-10 py-1 bg-[#1f1f1f] rounded-[28px] hover:opacity-90" onClick={handleLogOut}>I'm sure</button>
+                    <button className="px-4 md:px-10 py-1 bg-[#a90000] text-gray-300 rounded-[28px] hover:opacity-90" onClick={toggleLogoutPopUp}>Cancel</button>
+                    <button className="px-4 md:px-10 py-1 bg-[#1c1c1c] text-gray-300 rounded-[28px] hover:opacity-90" onClick={handleLogOut}>I'm sure</button>
                 </div>
             </div>
         </div>
