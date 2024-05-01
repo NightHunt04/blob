dc332d66ebe8ba47c6546ff901b59324d1cfb80f
admin1334 <jeetbherwani2004@gmail.com>
2024-05-01 11:36:36 +0530

    Separated chat completion models and image generative models and addeda title for the models in the main action page, also added other few models (integerating with their apis is not done)

--- src/components/MainActionPage/MainActionLanding/MainActionLanding.jsx
+++ src/components/MainActionPage/MainActionLanding/MainActionLanding.jsx
@@ -28,11 +28,21 @@ function MainActionLanding() {
                     {/* custom space */}
                     <div>
                         <p className='font-inter text-[15px] md:text-[20px] font-medium ml-3 py-2'>Models</p>
-                        <div className={`px-3 py-5 rounded-[17px] ${lightTheme ? 'bg-[#efefef]' : 'bg-[#2c2c2c]'} shadow-lg w-full md:w-[750px] mb-8`}>
-                            <div className='grid grid-cols-4 items-center justify-center gap-2 w-full md:w-[730px]'>
+                        <div className={`px-3 py-5 rounded-[17px] ${lightTheme ? 'bg-[#efefef]' : 'bg-[#2c2c2c]'} flex flex-col items-center justify-center shadow-lg w-full md:w-[750px] mb-8`}>
+
+                            {/* chat completion */}
+                            <p className='font-inter font-medium mb-6 text-[14px] md:text-[17px] px-5 py-2 rounded-[20px] bg-[#1e1e1e] text-center md:text-left w-[90%]'>Chat completion models</p>
+                            <div className='grid grid-cols-4 items-start justify-center gap-2 w-full md:w-[730px] border-b-[1px] pb-4 border-gray-600'>
                                 <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer hover:opacity-80 my-2' onClick={() => navigate('mistral-7b')}>
                                     <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full overflow-hidden border-[1px] border-[#737373] bg-[#1f1f1f] hover:bg-[#363636] hover:text-gray-300 transition-all'>
-                                        <img src="Assets/mistral-7b.jpeg" alt="Mistal" className='w-full h-full object-cover'/>
+                                        <img src="Assets/openhermes.jpg" alt="OpenHermes" className='w-full h-full object-cover'/>
+                                    </div>
+                                    <p className={`font-inter text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>OpenHermes</p>
+                                </div>
+
+                                <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer hover:opacity-80 my-2' onClick={() => navigate('mistral-7b')}>
+                                    <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full overflow-hidden border-[1px] border-[#737373] bg-[#1f1f1f] hover:bg-[#363636] hover:text-gray-300 transition-all'>
+                                        <img src="Assets/mistral-7b.jpeg" alt="Mistral" className='w-full h-full object-cover'/>
                                     </div>
                                     <p className={`font-inter text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>Mistral 7B</p>
                                 </div>
@@ -51,12 +61,25 @@ function MainActionLanding() {
                                     <p className={`font-inter text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>Gemini</p>
                                 </div>
 
+                                <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer hover:opacity-80 my-2' onClick={() => navigate('gemini')}>
+                                    <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full overflow-hidden border-[1px] border-[#737373] bg-[#1f1f1f] hover:bg-[#363636] hover:text-gray-300 transition-all'>
+                                        <img src="Assets/palm.png" alt="PaLM" className='w-full h-full object-cover'/>
+                                    </div>
+                                    <p className={`font-inter text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>PaLM</p>
+                                </div>
+
                                 <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer hover:opacity-80 my-2' onClick={() => navigate('llama')}>
                                     <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full overflow-hidden border-[1px] border-[#737373] bg-[#1f1f1f] hover:bg-[#363636] hover:text-gray-300 transition-all'>
                                         <img src="Assets/llama.jpeg" alt="LLama" className='w-full h-full object-cover'/>
                                     </div>
                                     <p className={`font-inter text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>LLama</p>
                                 </div>
+
+                            </div>
+
+                            <p className='font-inter mb-6 font-medium text-[14px] md:text-[17px] mt-[30px] md:mt-[40px] px-5 py-2 w-[90%] rounded-[20px] text-center md:text-left bg-[#1e1e1e]'>Image generative models</p>
+                            {/* image generators */}
+                            <div className='grid grid-cols-4 items-start justify-center gap-2 w-full md:w-[730px]'>
 
                                 <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer hover:opacity-80 my-2' onClick={() => navigate('dalle')}>
                                     <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full overflow-hidden border-[1px] border-[#737373] bg-[#1f1f1f] hover:bg-[#363636] hover:text-gray-300 transition-all'>
@@ -65,12 +88,20 @@ function MainActionLanding() {
                                     <p className={`font-inter text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>Dall-E</p>
                                 </div>
 
-                                <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer'>
-                                    <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full border-[1px] border-[#737373] bg-[#1f1f1f] hover:bg-[#363636] hover:text-gray-300 transition-all'>
-                                        <i className={`fa-solid fa-plus text-[26px] text-gray-300`}></i>
+                                <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer hover:opacity-80 my-2' onClick={() => navigate('dalle')}>
+                                    <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full overflow-hidden border-[1px] border-[#737373] bg-[#ce9000] hover:bg-[#ce9000e4] hover:text-gray-300 transition-all'>
+                                        <p className='font-inter font-semibold text-[14px] md:text-[20px] text-white'>SD</p>
                                     </div>
-                                    <p className={`font-inter text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>Add</p>
+                                    <p className={`font-inter text-[13px] pt-1 md:text-[16px] e-full text-center ${lightTheme ? 'text-black' : 'text-gray-300'}`}>Stable Diffusion</p>
                                 </div>
+
+                                <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer hover:opacity-80 my-2' onClick={() => navigate('dalle')}>
+                                    <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full overflow-hidden border-[1px] border-[#737373] bg-[#ce4f00] hover:bg-[#ce4f00e3] hover:text-gray-300 transition-all'>
+                                        <p className='font-inter font-semibold text-[14px] text-white md:text-[20px]'>SDXL</p>
+                                    </div>
+                                    <p className={`font-inter text-[13px] pt-1 md:text-[16px] e-full text-center ${lightTheme ? 'text-black' : 'text-gray-300'}`}>Stable Diffusion XL</p>
+                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -78,14 +109,10 @@ function MainActionLanding() {
                     {/* models for fun */}
                     <div>
                         <p className='font-inter text-[15px] md:text-[20px] font-medium ml-3 py-2'>Models for fun</p>
-                        <div className={`px-3 py-5 rounded-[17px] ${lightTheme ? 'bg-[#efefef]' : 'bg-[#2c2c2c]'} shadow-lg w-full md:w-[750px] mb-8`}>
+                        <div className={`px-3 py-5 rounded-[17px] ${lightTheme ? 'bg-[#efefef]' : 'bg-[#2c2c2c]'} flex flex-col items-center justify-center shadow-lg w-full md:w-[750px] mb-8`}>
+                            <p className='font-inter font-medium mb-6 text-[14px] md:text-[17px] px-5 py-2 rounded-[20px] bg-[#1e1e1e] w-[90%] text-center md:text-left'>Fine-tunned models</p>
+
                             <div className='grid grid-cols-4 items-start justify-center gap-2 w-full md:w-[730px]'>
-                                <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer my-2'>
-                                    <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full overflow-hidden border-[1px] border-[#737373] bg-[#1f1f1f] hover:bg-[#363636] hover:opacity-70 transition-all'>
-                                        <img src="Assets/ai-art.jpeg" alt="Rahul Gandhi" className='w-full h-full object-cover'/>
-                                    </div>
-                                    <p className={`font-inter text-center text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>Image Generator</p>
-                                </div>
 
                                 <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer my-2' onClick={() => navigate('rahul-gandhi')}>
                                     <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full overflow-hidden border-[1px] border-[#737373] bg-[#1f1f1f] hover:bg-[#363636] hover:opacity-70 transition-all'>
@@ -134,6 +161,13 @@ function MainActionLanding() {
                                         <img src="Assets/strange.jpeg" alt="Rahul Gandhi" className='w-full h-full object-cover'/>
                                     </div>
                                     <p className={`font-inter text-center text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>Dr. Strange</p>
+                                </div>
+
+                                <div className='flex flex-col items-center justify-center gap-1 hover:cursor-pointer'>
+                                    <div className='w-[40px] h-[40px] md:w-[60px] md:h-[60px] flex items-center justify-center rounded-full border-[1px] border-[#737373] bg-[#1f1f1f] hover:bg-[#363636] hover:text-gray-300 transition-all'>
+                                        <i className={`fa-solid fa-plus text-[26px] text-gray-300`}></i>
+                                    </div>
+                                    <p className={`font-inter text-[13px] pt-1 md:text-[16px] ${lightTheme ? 'text-black' : 'text-gray-300'}`}>Add</p>
                                 </div>
                             </div>
                         </div>
