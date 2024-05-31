bc4f5837d26139d41c4be3467bdeaf043f3d9b9e
admin1334 <jeetbherwani2004@gmail.com>
2024-05-31 20:08:41 +0530

    Changed the corner of setting pop up

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -6,7 +6,7 @@ function Settings() {
     const { toggleLogoutPopUp } = useModelContext()
 
     return (
-        <div className={`setting ${settings ? 'w-[300px] lg:w-[400px]' : 'w-0'} ${lightTheme ? 'text-[#343434] bg-[#cecece]' : 'text-[#e6e6e6] bg-[#363636]'} font-inter text-[15px] lg:text-[18px] overflow-hidden absolute transition-all shadow-2xl h-screen right-0 top-0 z-20`}>
+        <div className={`setting ${settings ? 'w-[300px] lg:w-[400px]' : 'w-0'} ${lightTheme ? 'text-[#343434] bg-[#cecece]' : 'text-[#e6e6e6] bg-[#363636]'} font-inter text-[15px] lg:text-[18px] overflow-hidden absolute transition-all shadow-2xl h-screen right-0 top-0 z-20 rounded-bl-xl`}>
             <div className={`flex transition-all relative flex-col items-center justify-start w-full h-full pt-[80px] md:pt-[120px]`}>
                 <div className="relative text-[13px] lg:text-[16px] text-center">
                     <Link to=''>
